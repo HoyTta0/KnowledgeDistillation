d706a04273aacb39d2fc933a09d7d0183582a603
HoyTra0 <893422529@qq.com>
2021-11-10 00:12:28 +0800

    修改模型结构

--- models/bert.py
+++ models/bert.py
@@ -1,44 +1,22 @@
 # coding: UTF-8
-import torch
 import torch.nn as nn
 import torch.nn.functional as F
-from transformers import BertModel, BertTokenizer
+from transformers import BertModel
 
 
-class Config(object):
-
-    """配置参数"""
-    def __init__(self, dataset):
-        self.class_list = [x.strip() for x in open(
-            dataset + '/class_multi1.txt').readlines()]                                # 类别名单
-        self.save_path = dataset + '/saved_dict/'  + '.ckpt'        # 模型训练结果
-        self.device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')   # 设备
-
-        self.require_improvement = 1000                                 # 若超过1000batch效果还没提升，则提前结束训练
-        self.num_classes = len(self.class_list)                         # 类别数
-        self.num_epochs = 10                                          # epoch数
-        self.batch_size = 64                                      # 128mini-batch大小
-        self.pad_size = 32                                           # 每句话处理成的长度(短填长切)
-        self.learning_rate =  5e-4                                    # 学习率
-        self.bert_path = './bert_pretrain'
-        self.tokenizer = BertTokenizer.from_pretrained(self.bert_path)
-        self.hidden_size = 768
-
-
-class Model(nn.Module):
+class BERT_Model(nn.Module):
 
     def __init__(self, config):
-        super(Model, self).__init__()
+        super(BERT_Model, self).__init__()
         self.bert = BertModel.from_pretrained(config.bert_path)
         for param in list(self.bert.parameters())[:-4]:
             param.requires_grad = False
         self.fc = nn.Linear(config.hidden_size, 192)
         self.fc1 = nn.Linear(192, config.num_classes)
 
-    def forward(self, x):
-        context = x[0]  # 输入的句子
-        mask = x[2]  # 对padding部分进行mask，和句子一个size，padding部分用0表示，如：[1, 1, 1, 1, 0, 0]
-        _, pooled = self.bert(context, attention_mask=mask)
+    def forward(self, context, mask):
+        outputs = self.bert(context, attention_mask=mask)
+        pooled = outputs[1]
         out = self.fc(pooled)
         out = F.relu(out)
         out = self.fc1(out)
--- models/biLSTM.py
+++ models/biLSTM.py
@@ -6,23 +6,28 @@
 # @File    : biLSTM.py
 # @Software: PyCharm
 """
+import torch
 import torch.nn as nn
 import torch.nn.functional as F
 
 
 class biLSTM(nn.Module):
-    def __init__(self):
+
+    def __init__(self, config):
         super(biLSTM, self).__init__()
-        self.lstm = nn.LSTM(input_size=300, hidden_size=768,
+        self.Embedding = nn.Embedding(21128,300)
+        self.lstm = nn.LSTM(input_size=300, hidden_size=300,
                             num_layers=1, batch_first=True, dropout=0, bidirectional=True)
         # self.linear = nn.Linear(in_features=256, out_features=2)
-        self.fc1 = nn.Linear(768*2, 192)
-        self.fc2 = nn.Linear(192, 5)
+        self.fc1 = nn.Linear(300*2, 192)
+        self.fc2 = nn.Linear(192, config.num_classes)
 
     def forward(self, x, hidden=None):
+        x = self.Embedding(x)
         lstm_out, hidden = self.lstm(x, hidden)     # LSTM 的返回很多
         out = self.fc1(lstm_out)
         activated_t = F.relu(out)
         linear_out = self.fc2(activated_t)
+        linear_out = torch.max(linear_out, dim=1)[0]
 
-        return linear_out, hidden
+        return linear_out
